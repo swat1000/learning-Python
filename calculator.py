2ac64202f006df70e9c4e8d73c53483fc3765c30
unknown <chaurasiyaswatantra65@gmail.com>
2022-12-07 12:24:44 +0530

    Calculator

---
+++ calculator.py
@@ -0,0 +1,11 @@
+
+
+
+num1 = int(input('Please input first number'))
+num2 = int(input('Please input second number'))
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1%num2)
