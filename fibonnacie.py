201584b584e888b932393dac3f6b15d3494dd6e5
unknown <chaurasiyaswatantra65@gmail.com>
2022-12-07 12:34:52 +0530

    Python practice

---
+++ fibonnacie.py
@@ -0,0 +1,4 @@
+sum=int(input('Enter Number of times'))
+for i in range (sum,2):
+    sum=sum+i
+    print(sum)
---
+++ get_oddevennumbers.py
@@ -0,0 +1,11 @@
+sum_odd=0
+sum_even=0
+for i in range (1,100,2):
+    sum_odd=sum_odd+i
+    print(sum_odd)
+
+print("----------------------------")    
+
+for i in range (0,100,2):
+    sum_even=sum_even+i
+    print(sum_even)    
